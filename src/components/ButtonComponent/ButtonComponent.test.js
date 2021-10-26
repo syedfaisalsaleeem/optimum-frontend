67039c304ea66b6df9476b9e2724b37d56a5346e
syedfaisalsaleem.100@gmail.com <syedfaisalsaleem.100@gmail.com>
2021-10-26 13:44:39 +0500

    rendering of all components unit test case passed

--- src/components/ButtonComponent/ButtonComponent.test.js
+++ src/components/ButtonComponent/ButtonComponent.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-// import ButtonComponent from './ButtonComponent.js';
+import ButtonComponent from './ButtonComponent.js';
 
 describe("Button Component",()=>{
     it("render button component",() => {
