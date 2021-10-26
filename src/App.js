d9534fd030c1dbe052a9d2abbbaf0f4c52812082
syedfaisalsaleem.100@gmail.com <syedfaisalsaleem.100@gmail.com>
2021-10-26 18:11:29 +0500

    unit test case failed for not having button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import InputBox from './components/InputBox/InputBox';
 import React from 'react';
+import ButtonComponent from './components/ButtonComponent/ButtonComponent';
+
 function App() {
   const [inputtext,changetextstate] = React.useState('');
   return (
     <>
+      <p> {inputtext}</p>
       <InputBox text={changetextstate} />
+      <ButtonComponent />
     </>
   );
 }
--- src/components/InputBox/InputBox.test.js
+++ src/components/InputBox/InputBox.test.js
@@ -5,10 +5,5 @@ import InputBox from './InputBox.js';
 describe("InputBox",()=>{
     it("render input box",() => {
         const inputbox = shallow(<InputBox/>);
-        const findinput = inputbox.find('input');
-        console.log(findinput.props)
-        findinput.props().onChange();
-        //   expect(component.state('input')).toEqual('myValue');
-        // expect(inputbox.getElements()).toMatchSnapshot();
     });
 });
