1fc20a232760c99fdb7bf1bde70d01547e598afb
syedfaisalsaleem.100@gmail.com <syedfaisalsaleem.100@gmail.com>
2021-10-27 13:46:00 +0500

    test case passed for input field is working

--- src/components/InputBox/InputBox.js
+++ src/components/InputBox/InputBox.js
@@ -2,7 +2,7 @@ const InputBox =(props) =>{
     console.log(props)
     return(
         <>
-         <input  type="text" onChange={e=> {}}/>
+         <input data-testid="text-input" type="text" onChange={e=> {}}/>
         </>
     )
 };
