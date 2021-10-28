488708da9bba8ba4cacacd7bf86f07c27b5d74e8
syedfaisalsaleem.100@gmail.com <syedfaisalsaleem.100@gmail.com>
2021-10-28 12:13:17 +0500

    integration and unit testing passed between components

--- src/components/TextBox/TextBox.js
+++ src/components/TextBox/TextBox.js
@@ -1,16 +1,28 @@
-const TextBox =() =>{
-    const todolist = []
-    return(
-        <>
-        <ul>
-            {todolist.map((item)=>{
-                return (<li>
-                    {item}
-                </li>);
-            })}
-        </ul>
-        </>
-    )
+const TextBox =(props) =>{
+    console.log(props.todolist)
+    const todolist = props.todo_list
+    if (todolist){
+        return(
+            <>
+            <ul>
+                {todolist.map((value,index)=>{
+                    return (<li key={index}>
+                        {value}
+                    </li>);
+                })}
+            </ul>
+            </>
+        )
+    }
+    else{
+        return(
+            <>
+            <ul>
+            </ul>
+            </>
+        )
+    };
+
 };
 
 export default TextBox;
