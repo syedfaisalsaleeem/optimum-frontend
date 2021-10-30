bf609dd3df7dec5d906d844ae970faad7c770a55
syedfaisalsaleeem <syedfaisalsaleem.100@gmail.com>
2021-10-31 00:05:22 +0100

    contract test passed for create todoitem list

--- src/EventsClient.js
+++ src/EventsClient.js
@@ -23,6 +23,27 @@ class EventsClient {
         return response.data
       });
   }
+
+  createEvents(body) {
+    const headers = {"Accept": "application/json"};
+    // return {
+    //     todolist: 'Is it possible to export projects on this app?'
+    // };
+    var path=require('path');
+    var lib=path.join(path.dirname(require.resolve('axios')),'lib/adapters/http');
+    var http=require(lib);
+    return axios({
+        adapter: http,
+        url: `${this.host}/todolist`,
+        method: 'POST',
+        data:body,
+        headers
+      })
+      .then(function (response) {
+        return response.data
+      });
+  }
+
 }
 
 export default EventsClient;
