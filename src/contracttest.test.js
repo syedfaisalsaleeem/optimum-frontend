3045c759f1da66e4ef3d01f93f2e4145abcf4a13
syedfaisalsaleeem <syedfaisalsaleem.100@gmail.com>
2021-10-31 00:04:37 +0100

    contract test failed for create todoitem list

--- src/contracttest.test.js
+++ src/contracttest.test.js
@@ -3,11 +3,6 @@ import { Matchers } from '@pact-foundation/pact';
 import EventsClient from './EventsClient';
 
 pactWith({ consumer: 'MyConsumer', provider: 'MyProvider' }, provider => {
-  let client;
-
-//   beforeEach(() => {
-//     client = api('http://your-api.example.com').ge
-//   });
 
   describe('get list of todo', () => {
     const feedback = [{
@@ -38,17 +33,6 @@ pactWith({ consumer: 'MyConsumer', provider: 'MyProvider' }, provider => {
       })
     );
 
-    // You also test that the API returns the correct
-    // response to the data layer.
-    //
-    // Although Pact will ensure that the provider
-    // returned the expected object, you need to test that
-    // your code recieves the right object.
-    //
-    // This is often the same as the object that was
-    // in the network response, but (as illustrated
-    // here) not always.
-
     it('should get list of todo items', async() => {
         // // Arrange
         const feedbackClient = new EventsClient({host: provider.mockService.baseUrl});
@@ -60,11 +44,51 @@ pactWith({ consumer: 'MyConsumer', provider: 'MyProvider' }, provider => {
         expect(result).toEqual(feedback);
         provider.verify(result);
     });
-    // it('returns server health', () => // implicit return again
-    //   client.health().then(health => {
-    //     expect(health).toEqual('up');
-    //   }
-    //   )
-    //   );
   });
+
+
+  describe('create todo item', () => {
+    const feedback = [{
+        todolist: 'Is it possible to export projects on this app?'
+    }];
+    const expectedResult = [{
+        todolist: 'Is it possible to export projects on this app?'
+    }];
+    // Here we set up the interaction that the Pact
+    // mock provider will expect.
+    //
+    // jest-pact takes care of validating and tearing
+    // down the provider for you.
+    beforeEach(() => // note the implicit return.
+                     // addInteraction returns a promise.
+                     // If you don't want to implict return,
+                     // you will need to `await` the result
+      provider.addInteraction({
+        uponReceiving: 'A request to create todo item',
+        willRespondWith: {
+          status: 201,
+          headers: { 'Content-Type': 'application/json' },
+          body: expectedResult,
+        },
+        withRequest: {
+          method: 'POST',
+          headers: { 'Accept': 'application/json' },
+          path: '/todolist',
+        },
+      })
+    );
+
+    it('should create list of todo items', async() => {
+        // // Arrange
+        const feedbackClient = new EventsClient({host: provider.mockService.baseUrl});
+
+        // // Act
+        const result = await feedbackClient.createEvents(feedback);
+        // Assert
+        expect(result).toEqual(expectedResult);
+        provider.verify(result);
+    });
+
+  });
+
 });
